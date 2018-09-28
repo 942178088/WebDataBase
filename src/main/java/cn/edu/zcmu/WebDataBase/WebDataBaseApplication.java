4c8ac39fc0964519548f87a5dd07730f0794345c
汪恒 <782715416@qq.com>
2018-09-28 19:05:55 +0800

    配置Spring Boot

---
+++ src/main/java/cn/edu/zcmu/WebDataBase/WebDataBaseApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.zcmu.WebDataBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDataBaseApplication.class, args);
+	}
+}
